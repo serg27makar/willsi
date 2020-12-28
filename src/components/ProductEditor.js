653185b7fbb59680386c93f41570eb7fb5106353
Serg <serg27makar@gmail.com>
2020-12-28 10:47:27 +0200

    Added search block

--- src/components/ProductEditor.js
+++ src/components/ProductEditor.js
@@ -4,14 +4,66 @@ import ru from "../access/lang/LangConstants";
 import {actionSelectedProductToEdit} from "../action";
 import AddButton from "./adminPanel/AddButton";
 
+const searchItemName = [
+    "Manufacturer", "ProdName", "ProductCode"
+]
 class ProductEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            Manufacturer: "",
+            ProdName: "",
+            ProductCode: "",
+            list: [],
+        };
+        this.dataOnChange = this.dataOnChange.bind(this);
+        this.searchItem = this.searchItem.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.list) this.fillList();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.list !== this.props.list) this.fillList();
+    }
+
+    fillList() {
+        this.setState({
+            ...this.state,
+            list: this.props.list,
+        })
     }
 
     selectedProductToEdit(item) {
         this.props.selectedProductToEditFunction(item);
+    }
+
+    dataOnChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.searchItem(name, value)
+    }
+
+    searchItem(name, value) {
+        const list = [];
+        this.props.list.map(item => {
+            if (item[name].toUpperCase().indexOf(value.toUpperCase()) !== -1) {
+                list.push(item);
+            }
+        })
+        let clearData = this.state;
+        clearData = {
+            ...clearData,
+            Manufacturer: "",
+            ProdName: "",
+            ProductCode: "",
+        }
+        this.setState({
+            ...clearData,
+            [name]: value,
+            list,
+        })
     }
 
     renderListEditor(item, index) {
@@ -25,6 +77,35 @@ class ProductEditor extends React.Component {
         )
     }
 
+    renderSearchInput(item, index) {
+        return (
+            <div className="stroke-descriptor-manufacturer" key={index}>
+                <label className="search-label">
+                    <input className="search-input text-14 light"
+                           type={"text"}
+                           name={item}
+                           value={this.state[item]}
+                           onChange={this.dataOnChange}
+                    />
+                </label>
+            </div>
+        )
+    }
+
+    renderSearchBlock() {
+        return (
+            <div className="stroke-descriptor-wrapper">
+                <div className="stroke-descriptor-number text-14"/>
+                {searchItemName.map((item, index) => {
+                    return this.renderSearchInput(item, index);
+                })}
+                <svg className="search-label icon">
+                    <use xlinkHref={"static/img/svg-sprites/symbol/sprite.svg#search"}/>
+                </svg>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
@@ -34,7 +115,8 @@ class ProductEditor extends React.Component {
                     <div className="stroke-descriptor-name text-14">{ru.ProdName}</div>
                     <div className="stroke-descriptor-code text-14">{ru.ProductCode}</div>
                 </div>
-                {this.props.list && this.props.list.map((item, index) => {
+                {this.renderSearchBlock()}
+                {this.state.list && this.state.list.map((item, index) => {
                     return this.renderListEditor(item, index);
                 })}
                 <AddButton lastItem={true} addProduct={this.props.addProduct}/>
