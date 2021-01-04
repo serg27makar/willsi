0aad97ab11af2d01c5954d803de9b6f2db2c5e13
Serg <serg27makar@gmail.com>
2021-01-04 10:32:50 +0200

    Remove recalculate footer block before data filling

--- src/components/RecalculateFooter.js
+++ src/components/RecalculateFooter.js
@@ -24,11 +24,12 @@ class RecalculateFooter extends React.Component {
                 <Redirect to={"/catalog"}/>
             )
         }
+        if (this.props.disabled) return null;
         return (
             <div className="col-12 recalculate-footer">
                 <div className="col-12">
-                    <button className={"recalculate__button text-22 medium button-main" + (this.props.disabled ? " disabled-btn" : "")}
-                            disabled={this.props.disabled} onClick={this.redirect}>{ru.Count}</button>
+                    <button className="recalculate__button text-22 medium button-main"
+                            onClick={this.redirect}>{ru.Count}</button>
                 </div>
                 <div className="col-12">
                     <p className="recalculate__bottom-title title-36 uppercase color-aqua bold">{ru.YouAreAmazing}</p>
